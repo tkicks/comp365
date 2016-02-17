63ccad87bee8aecc2680706c472104235aa05e3c
Tyler <tkicks32@gmail.com>
2016-02-16 21:33:55 -0500

    quickpush

--- hw/g2/kickhamG2.cpp
+++ hw/g2/kickhamG2.cpp
@@ -14,10 +14,14 @@ Input:		The user will input using the mouse
 Output:		A computer graphic window will be displayed which will initially
 			be empty.  With each keypress (as explained in input) the
 			fractal will expand or shrink (and maybe change colors).
+
+
+TODO: fix setInMenu
 */
 
 #include <GL/glut.h>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class dragonFractal
@@ -25,13 +29,40 @@ class dragonFractal
 	public:
 		dragonFractal() {};
 		~dragonFractal() {};
+		void drawFractal();				// function to draw fractal
+		bool getInMenu();				// return if left click happened in menu
+		void setInMenu(bool);			// set inMenu value to true or false
 	private:
-		int levels;
+		int levels;						// number of levels in the fractal
+		bool inMenu;			// was left click in menu?
+		vector<float> fractalPoints;	// vector of points in fractal
 };
 
 const int iterationLimit = 7;		// maximum number of iterations allowed
 const float ww = 500.0;				// set window's width
 const float wh = 500.0;				// set window's height
+dragonFractal fractal;				// create an object to use
+
+void dragonFractal::drawFractal()
+// INPUT: none?	OUTPUT: none
+// draw next fractal level
+{
+	cout << "Draw fractal\n";
+}
+
+void dragonFractal::setInMenu(bool value)
+// INPUT: true or false bool value	OUTPUT: none
+// set the value of in menu to true or false
+{
+	this->inMenu = value;
+}
+
+bool dragonFractal::getInMenu()
+// INPUT: none	OUTPUT: true if clicked while in menu false if clicked out of menu
+// checks what to return for output
+{
+	return this->inMenu;
+}
 
 
 void display ()
@@ -51,9 +82,9 @@ void mouse (int button, int state, int x, int y)
 // OUTPUT: none
 // increase fractal level
 {
-	// holder
-	if (state == GLUT_UP)
-		cout << "left mouse clicked\n";
+	// if the left mouse was pressed and it wasn't in the menu
+	if (state == GLUT_UP && !fractal.getInMenu())
+		fractal.drawFractal();
 }
 
 void menu (int menuVal)
@@ -68,6 +99,7 @@ void menu (int menuVal)
 		case 2:
 		case 3:
 		case 4: cout << "Menu option " << menuVal << " chosen\n";
+				fractal.setInMenu(false);
 				break;
 		case 5: exit(1);
 	}
@@ -91,9 +123,10 @@ void init ()
 }
 
 void initMenu ()
-// INPUT: none	OUTPUT: none
+// INPUT: fractal object	OUTPUT: none
 // initialize the menu on a right mouse click
 {
+	fractal.setInMenu(true);		// say in menu
 	glutCreateMenu(menu);			// call the menu function
 	glutAddMenuEntry("Create New (default)", 0);
 	glutAddMenuEntry("Create New (custom)", 1);
