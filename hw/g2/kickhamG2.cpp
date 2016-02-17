818a980922bfe750c74b7058ee5d332371e32cad
Tyler <tkicks32@gmail.com>
2016-02-16 19:01:03 -0500

    g2 comment

---
+++ hw/g2/kickhamG2.cpp
@@ -0,0 +1,11 @@
+/*
+Name:		Tyler Kickham
+Program:	Dragon Fractal
+Purpose:	Use C++ and OpenGL to create an interactive dragon fractal
+Input:		The user will input using the keyboard
+			to: right key - iterate another level of the fractal
+				left key - remove a level of the fractal
+Output:		A computer graphic window will be displayed which will initially
+			be empty.  With each keypress (as explained in input) the
+			fractal will expand or shrink (and maybe change colors).
+*/
