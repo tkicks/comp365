6680affc1180119076fe04547584f9bce8026a82
Tyler <tkicks32@gmail.com>
2016-02-18 23:33:12 -0500

    quickpush

--- hw/g2/kickhamG2.cpp
+++ hw/g2/kickhamG2.cpp
@@ -14,6 +14,9 @@ Input:		The user will input using the mouse
 Output:		A computer graphic window will be displayed which will initially
 			be empty.  With each keypress (as explained in input) the
 			fractal will expand or shrink (and maybe change colors).
+
+set segment length by keyboard input of number of pixels
+(1-9 unless figure out how else)
 */
 
 #include <GL/glut.h>
@@ -26,16 +29,22 @@ class dragonFractal
 	public:
 		dragonFractal() {};
 		~dragonFractal() {};
-		void drawFractal();				// function to draw fractal
-		bool getInMenu();				// return if left click happened in menu
-		void setInMenu(bool);			// set inMenu value to true or false
+		void drawFractal();							// function to draw fractal
+		bool getInMenu();							// return if left click happened in menu
+		void setInMenu(bool);						// set inMenu value to true or false
+		void setNext(float x, float y);				// set start x and y coords
+		void setLength(float length);				// set segment length
 	private:
-		int levels;						// number of levels in the fractal
-		bool inMenu;					// was left click in menu?
-		vector<float> fractalPoints;	// vector of points in fractal
+		int levels;									// number of levels in the fractal
+		int pointNum;								// number of points it's on for vector iterations
+		float location;								// initial location
+		float segLength;							// length of segments
+		bool inMenu;								// was left click in menu?
+		vector<vector <float> > fractalPoints;		// vector of points in fractal [i][0] = x [i][1] = y
+		vector<float> tempVector;					// intermediary vector for 2d vector
 };
 
-const int iterationLimit = 7;		// maximum number of iterations allowed
+const int iterationLimit = 3;		// maximum number of iterations allowed
 const float ww = 500.0;				// set window's width
 const float wh = 500.0;				// set window's height
 dragonFractal fractal;				// create an object to use
@@ -44,7 +53,38 @@ void dragonFractal::drawFractal()
 // INPUT: none?	OUTPUT: none
 // draw next fractal level
 {
-	cout << "Draw fractal\n";
+	// cout << "Draw fractal\n";
+	cout << fractalPoints.size() << endl;
+	// if it's the first point, draw another a segment length to the right
+	if (fractalPoints.size() == 1)
+	{
+		fractal.setNext(fractalPoints[0][0], (fractalPoints[0][1] + segLength));
+	}
+	else
+	{
+		// fractal.setNext(fractalPoints[pointNum][0])
+	}
+}
+
+void dragonFractal::setNext(float x, float y)
+// INPUT: float x and y coords to start at 	OUTPUT: none
+// set the starting x and y coords
+{
+	this->tempVector.push_back(x);				// next spot in vector gets x coord
+	this->tempVector.push_back(y);				// then y coord of next point
+	this->fractalPoints.push_back(tempVector);	// push new coords to 2d vector of points
+	this->tempVector.clear();					// empty out intermediary vector so no repeats
+
+	// test storage
+	// for (int i = 0; i < fractalPoints.size(); i++)
+	// 	cout << fractalPoints[i][0] << " " << fractalPoints[i][1] << endl;				// see what's stored
+}
+
+void dragonFractal::setLength(float length)
+// INPUT; float segment length (1-9)	OUTPUT: none
+// sets the segment length (can be modified on the fly)
+{
+	this->segLength = length;
 }
 
 void dragonFractal::setInMenu(bool value)
@@ -119,6 +159,11 @@ void init ()
 
 	// define coordinate system in x, y 0-1
 	gluOrtho2D (0.0, ww, 0.0, wh);
+
+	// set default starting location to center of screen (250, 250)
+	fractal.setNext(250.0, 250.0);
+	// set default segment length to 5
+	fractal.setLength(5.0);
 }
 
 void initMenu ()
